c794f12e8d11641c937eaf759dd6da78aa71cd20
code-yy <jiyeyouren523@gmail.com>
2022-09-11 15:54:39 +0900

    :recycle: 修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
       <SEO
         title="ユウトブログ"
         description="技術ブログや、近況報告などをやっていきます"
-        url="https://code-yy-blog.vercel.app/god.png"
+        url="https://lh3.googleusercontent.com/wR7ElVGqrF2okr0ibllfrVAA5-uEKcrXjr4xDjX9lA4YC4f_8x8_jl6vLDSAfbOFMn5uDemDz2q-6-h-pwbHLV_0GaZjl04pZ7UhpYw=s0"
       />
       {Layout(<Component {...pageProps} />)}
     </>
