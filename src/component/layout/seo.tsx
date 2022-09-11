f878c1bdf9a5dc714e6a9a3330ceab1a995ec074
code-yy <jiyeyouren523@gmail.com>
2022-09-11 16:49:20 +0900

    :sparkles: ブログページ修正

--- src/component/layout/seo.tsx
+++ src/component/layout/seo.tsx
@@ -2,20 +2,29 @@ import { FC } from "react";
 import NextHead from "next/head";
 
 type Props = {
-  title: string;
-  description: string;
-  url: string;
+  title?: string;
+  description?: string;
+  url?: string;
+  type?: "blog" | "article";
 };
 
-export const SEO: FC<Props> = ({ title, description, url }) => {
+export const SEO: FC<Props> = ({ title, description, url, type = "blog" }) => {
   return (
     <NextHead>
       <title>ユウトブログ</title>
+      <link rel="icon" href="/god.png" />
+      <meta name="description" content="技術ブログや、近況報告などをやっていきます" />
+
+      {/* Normal */}
       <meta property="og:title" content={title} />
+      <meta property="og:type" content={type} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={url} />
+
+      {/* Twitter */}
       <meta property="twitter:title" content={title} />
       <meta property="twitter:image" content={url} />
+      <meta property="twitter:description" content={description} />
       <meta name="twitter:card" content="summary_large_image" />
     </NextHead>
   );
