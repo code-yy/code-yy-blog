48346f722124fe94a1ced467242c303f88da44c0
code-yy <jiyeyouren523@gmail.com>
2022-09-11 19:24:28 +0900

    fix

--- src/component/layout/head.tsx
+++ src/component/layout/head.tsx
@@ -11,7 +11,7 @@ export const Head: FC<Props> = ({ title, description, url }) => {
   const _title = title ?? "ユウトブログ";
   const _description: string = description ?? "技術ブログや、近況報告などをやっていきます";
   const _url: string =
-    `https://code-yy-blog.vercel.app/${url}` ??
+    url ??
     "https://lh3.googleusercontent.com/wR7ElVGqrF2okr0ibllfrVAA5-uEKcrXjr4xDjX9lA4YC4f_8x8_jl6vLDSAfbOFMn5uDemDz2q-6-h-pwbHLV_0GaZjl04pZ7UhpYw=s0";
 
   return (
