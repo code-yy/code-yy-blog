3bb0d27b139402f6a2c07bf8ad10c4b85df3023c
code-yy <jiyeyouren523@gmail.com>
2022-09-11 15:28:01 +0900

    :bug: _app.tsxにcsr状態のページのhead情報を移動

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
+import { Head } from "@/component/layout";
 import { AppPropsWithLayout } from "@/type/next-type";
 import "src/style/globals.css";
 
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const Layout = Component.getLayout ?? ((page) => page);
 
-  return Layout(<Component {...pageProps} />);
+  return (
+    <>
+      <Head
+        title="ユウトブログ"
+        description="技術ブログや、近況報告などをやっていきます"
+        url="https://code-yy-blog.vercel.app/god.png"
+        twitterCard="summary_large_image"
+      />
+      {Layout(<Component {...pageProps} />)}
+    </>
+  );
 };
 
 export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,10 @@
-import { Html, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 import { NextPage } from "next";
-import { Head } from "@/component/layout";
 
 const Document: NextPage = () => {
   return (
     <Html>
-      <Head
-        title="ユウトブログ"
-        description="技術ブログや、近況報告などをやっていきます"
-        url="https://code-yy-blog.vercel.app/god.png"
-        twitterCard="summary_large_image"
-      />
+      <Head />
       <body>
         <Main />
         <NextScript />
