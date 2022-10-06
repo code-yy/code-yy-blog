781bda08787751859dfb35cabdfee57347085ce5
code-yy <jiyeyouren523@gmail.com>
2022-10-06 15:13:05 +0900

    🎨 ogpの修正

--- src/pages/blogs/[slug].tsx
+++ src/pages/blogs/[slug].tsx
@@ -34,11 +34,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 const BlogIdPage: NextPage<Props> = ({ blog }) => {
   return (
     <>
-      <Head
-        title={`ユウトブログ/${blog.title}`}
-        description={blog.description}
-        url={`${blog.eyecatch.url}?w=1200&h=630`}
-      />
+      <Head title={blog.title} description={blog.description} url={`${blog.eyecatch.url}?w=1200&h=630`} />
       <Layout>
         <BlogId blog={blog} />
       </Layout>
