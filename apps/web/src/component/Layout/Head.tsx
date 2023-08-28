b8d25172a2475a5a732fd453259b6d9840be593e
yossydev <codeyy021@gmail.com>
2023-08-28 21:33:10 +0900

    fix: To be able to display images set by ogp

--- apps/web/src/component/Layout/Head.tsx
+++ apps/web/src/component/Layout/Head.tsx
@@ -4,9 +4,10 @@ import NextHead from "next/head";
 type Props = {
   title?: string;
   description?: string;
+  imageUrl?: string;
 };
 
-export const Head: FC<Props> = ({ title, description }) => {
+export const Head: FC<Props> = ({ title, description, imageUrl }) => {
   const _title: string = title ?? "ユウトブログ";
   const _description: string = description ?? "技術ブログや、近況報告などをやっていきます";
   const _url = "https://user-images.githubusercontent.com/87469023/233515022-26b256e8-8208-4a62-8621-aea4a9359742.png";
@@ -20,11 +21,11 @@ export const Head: FC<Props> = ({ title, description }) => {
         {/* Main */}
         <meta property="og:title" content={_title} />
         <meta property="og:description" content={_description} />
-        <meta property="og:image" content={_url} />
+        <meta property="og:image" content={imageUrl ?? _url} />
 
         {/* Twitter */}
         <meta name="twitter:site" content="@codeyy_dev" />
-        <meta name="twitter:image" content={_url} />
+        <meta name="twitter:image" content={imageUrl ?? _url} />
         <meta name="twitter:card" content="summary" />
       </NextHead>
     </>
