23af0f0e05dc956a5130cd335bb0d71bfd4700db
yossydev <codeyy021@gmail.com>
2023-08-28 22:07:49 +0900

    fix: prefix it with domain

--- apps/web/src/component/Layout/Head.tsx
+++ apps/web/src/component/Layout/Head.tsx
@@ -11,6 +11,7 @@ export const Head: FC<Props> = ({ title, description, imageUrl }) => {
   const _title: string = title ?? "ユウトブログ";
   const _description: string = description ?? "技術ブログや、近況報告などをやっていきます";
   const _url = "https://user-images.githubusercontent.com/87469023/233515022-26b256e8-8208-4a62-8621-aea4a9359742.png";
+  const _image = `https://yossydev.com${imageUrl ?? _url}`;
 
   return (
     <>
@@ -21,12 +22,12 @@ export const Head: FC<Props> = ({ title, description, imageUrl }) => {
         {/* Main */}
         <meta property="og:title" content={_title} />
         <meta property="og:description" content={_description} />
-        <meta property="og:image" content={imageUrl ?? _url} />
+        <meta property="og:image" content={_image} />
 
         {/* Twitter */}
         <meta name="twitter:site" content="@codeyy_dev" />
-        <meta name="twitter:image" content={imageUrl ?? _url} />
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:image" content={_image} />
+        <meta name="twitter:card" content="summary_large_image" />
       </NextHead>
     </>
   );
